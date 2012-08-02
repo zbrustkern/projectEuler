93cd21eccd022a226215b6e91f89a38878d03a38
Zeke Brustkern <zbrustkern@gmail.com>
2012-08-01 20:56:00 -0400

    problem five solved

---
+++ oneToTwenty.py
@@ -0,0 +1,15 @@
+
+def lcm(a, b):
+	n = a
+	while n % b != 0:
+		n += a
+	return n
+
+def oneToTwenty():
+	num = lcm(11,12)
+
+	for i in range(13, 20, 1):
+		num = lcm(num, i)
+	return num
+
+print oneToTwenty()
